e5b78aaf08581704a050ee68a958d856f6b42e83
Kenneth Wright <ten53@Mac.fritz.box>
2026-06-18 10:16:13 +0200

    Restrict node to only listen on the localhost network interface

--- src/server.js
+++ src/server.js
@@ -20,6 +20,6 @@ const port = 3000
 
 const server = createServer(port)
 
-server.listen(port, () => {
-    console.log(`Server is running on ${port}`);
+server.listen(port, '127.0.0.1', () => {
+    console.log(`Server is running on http://127.0.0.1:${port}`);
 })
